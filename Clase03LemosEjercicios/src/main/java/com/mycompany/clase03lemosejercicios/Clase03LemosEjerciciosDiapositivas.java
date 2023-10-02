0b47a1792cf57cf0eb9d8ecb8c161c5e6655637c
María Paula Lemos <paulalemosstafe@gmail.com>
2023-10-02 06:34:52 -0300

    Finalizado Ejercicios Diapositivas
    Se termino de escribir los ejercicios que aparecen en las diapositivas de la clase 03

---
+++ Clase03LemosEjercicios/src/main/java/com/mycompany/clase03lemosejercicios/Clase03LemosEjerciciosDiapositivas.java
@@ -0,0 +1,30 @@
+package com.mycompany.clase03lemosejercicios;
+
+public class Clase03LemosEjerciciosDiapositivas {
+
+    public static void main(String[] args) {
+        System.out.println("Clase 03: Ejercicios de las Diapositivas");
+        
+        /* Del lado izquierdo del igual se declara la variable y del lado derecho se crea el array,
+            el número dentro de los corchetes indica la cantidad de elementos que va a tener el array*/
+        int numeros[] = new int[3]; // Asi se declara un arreglo o array que tendrá elementos que serán números enteros
+        
+        // Asignaciones:
+        numeros[0] = 1;
+        numeros[1] = 37;
+        numeros[2] = 16;
+        
+        // Tambien puede inicializaarse directo con valores
+        int numeros2[] = new int[] {1, 37, 16} ;
+        
+        System.out.println(numeros.length); // Imprime por pantalla la cantidad de elementos que tiene el array
+        
+        /* La sintaxis de punto (".") indica que estamos accediendo a una propiedad de la variable. Lo que vimos como 
+            "tipos primitivos" no lo permiten, pero los ARRAYS tienen una fundamental: "LENGTH", que nos dice el tamaño del
+            ARRAY. */
+        
+        for (int indice = 0; indice < indice; indice++) {
+            System.out.println(otroNum); // Imprime por pantalla cada elemento
+        }
+    }
+}
